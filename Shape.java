da482dc777b4ee5ab6cda94c6611a143ef4c0e5a
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-30 19:29:59 +0500

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -7,18 +7,23 @@ public class Shape {
 public Shape(String Color){
     
     this.Color=Color;
+    
 }    
     
 public String toString(){
     
     return "Shape[Color = "+ Color +"]";
+    
 }
 
 public Double GetArea(){
     
     System.err.println("Shape Unknown , Cant Find Area ");
+    
     return 0.0;
+    
 }
+    
 }
 
 class Rectangle extends Shape{
@@ -26,7 +31,6 @@ class Rectangle extends Shape{
     private int Lenght;
     private int Width;
     
-
 
 public Rectangle(String Color, int lenght, int width){
 
@@ -37,16 +41,21 @@ this.Width=width;
 }
 
 @Override
+    
 public String toString(){
     
     return "Rectangle[Lenght = "+ Lenght +",Width = "+ Width +","+super.toString()+"]";
+    
 }
 
 @Override
+    
 public Double GetArea(){
 
     return(double) Lenght*Width;
+    
 }
+    
 }
 
 class Triangle extends Shape{
@@ -60,17 +69,21 @@ public Triangle(String Color, int Base, int Hieght){
     super(Color);
     this.Base=Base;
     this.Hieght=Hieght;
+    
 }
 
 public String toString(){
     
     return "Triangle[Base = "+ Base +", Hieght = "+ Hieght +", "+super.toString()+"]";
+    
 }
 
 @Override
+    
 public Double GetArea(){
     
     return(Double) 0.5*Base*Hieght;
+    
 }
 
 }
